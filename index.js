3358ab7c3eaf0e02ef164ffee77afe1d2a5d5246
Sarah Diener <sdiener@ksu.edu>
2022-07-21 23:40:19 -0500

    Added heroku

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.use(cors())
 app.get('/api', (req, res) => {
   res.status(200).json({api: 'version 1'})
 })
-
-app.listen(3000, () => console.log('server listening on port 3000'))
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log('server started on port', port))
+//app.listen(3000, () => console.log('server listening on port 3000'))
 
 
 
